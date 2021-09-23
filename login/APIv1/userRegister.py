7f25ff494ea2e1e4119fe7450d805c986fa77f0c
aotella <prkrkmr@hotmail.com>
2021-09-24 00:34:48 +0530

    Deleted unwanted files

--- login/APIv1/userRegister.py
+++ login/APIv1/userRegister.py
@@ -3,24 +3,24 @@ import bcrypt
 
 
 def userRegister(request):
-	userName = request['userName']
-	age = request['userAge']
-	country = request['userCountry']
-	email = request['emailID']
-	password = str(request['userPassword'])
-	password = bcrypt.hashpw(str(password), bcrypt.gensalt())
-	userObj = UserModel()
-	userObj.UserName = userName
-	userObj.Age = age
-	userObj.Country = country
-	userObj.EmailID = email
-	userObj.Password = password
+    userName = request["userName"]
+    age = request["userAge"]
+    country = request["userCountry"]
+    email = request["emailID"]
+    password = str(request["userPassword"])
+    password = bcrypt.hashpw(str(password), bcrypt.gensalt())
+    userObj = UserModel()
+    userObj.UserName = userName
+    userObj.Age = age
+    userObj.Country = country
+    userObj.EmailID = email
+    userObj.Password = password
 
-	try:
-		userObj.save()
-		userStatObj = UserStat()
-		userStatObj.UserName = UserModel.objects.get(UserName = request['userName'])
-		userStatObj.save()
-		return 202
-	except:
-		return 400
+    try:
+        userObj.save()
+        userStatObj = UserStat()
+        userStatObj.UserName = UserModel.objects.get(UserName=request["userName"])
+        userStatObj.save()
+        return 202
+    except:
+        return 400
